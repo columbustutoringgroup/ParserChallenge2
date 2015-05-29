8b81f18e886aaf5a5d52433df1356ac47e9acfd3
Lovinder Pnag <lspnag@gmail.com>
2015-05-28 23:39:16 -0400

    working solution with all tests passing but needs significant refactoring

--- parser.rb
+++ parser.rb
@@ -15,26 +15,35 @@ require 'pry'
 	# private
 
 	def fouls(w1,w2)
-		array_w1 = w1.split("")
-		array_w2 = w2.split("")
+		array_w1 = w1.chars
+		array_w2 = w2.chars
 		array_w1.each do |x|
 			# if array_w2.include?(x)
 			# 	array_w1.delete(x)
 			# end
 				array_w2.delete(x)
+				# array_w1.delete(x)
 			
 		end
-		# arrxay_w2
-		# array_w1 - array_w2
+		z = 0
+		# z = 1 if jumbled(w1,w2) != 0 && w1.size != w2.size
+		# z = 1 if array_w2.count >= 1
 
-		array_w2.count + jumbled(w1,w2) 
+		wd1 = w1.chars
+		wd2 = w2.chars
+
+		z = 1 if wd1.last == wd2.last && wd1.first == wd2.first && w1.size != w2.size
+
+		# p z
+		# p array_w2
+		array_w2.count + jumbled(w1,w2) + z
 
 	end
 
 	def jumbled(w1,w2)
-		if w1.split("").last == w2.split("").last
+		if w1.chars.last == w2.chars.last
 			0
-		elsif w1.split("").first != w2.split("").first
+		elsif w1.chars.first != w2.chars.first
 			2
 		else
 			1
@@ -43,10 +52,12 @@ require 'pry'
 # end
 
 
-p fouls("cawffee", "coffee")  # 1
-p fouls("doge", "dog")
-# p kinda_like?("doge", "dog")
-# p kinda_like?("flow", "wolf")
+# p fouls("cawffee", "coffee")  # 1
+# p fouls("bog", "dog")
+p kinda_like?("doge", "dog") == true
+p kinda_like?("bog", "dog") == true
+p kinda_like?("flow", "wolf") == false
+p kinda_like?("cawffee", "coffee") == false
 
 # test = Advparser.new
 # p test.fouls("bog", "dog")

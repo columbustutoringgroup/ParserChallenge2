a207c8eda164abafea941eb3ae7b9be76f226608
LoRyder1 <lpnag@CMDVMOSXLT075.local>
2015-05-29 13:35:41 -0400

    this should work

--- parser.rb
+++ parser.rb
@@ -1,38 +1,36 @@
 require 'pry'
 
 def kinda_like?(fuzzy_word, exact_word)
-	if fuzzy_word == exact_word
-		return true
-	elsif fouls(fuzzy_word, exact_word) < 2
-		return true
-	else 
-		return false
-	end
+  if fuzzy_word == exact_word
+    return true
+  elsif fouls(fuzzy_word, exact_word) < 2
+    return true
+  else 
+    return false
+  end
 end
 
 def fouls(w1,w2)
-	array_w1 = w1.chars
-	array_w2 = w2.chars
-	array_w1.each do |x|
-		array_w2.delete(x)
-	end
-	z = 0
-	wd1 = w1.chars
-	wd2 = w2.chars
-	z = 1 if wd1.last == wd2.last && wd1.first == wd2.first && w1.size != w2.size
-	array_w2.count + jumbled(w1,w2) + z
+  array_w2 = w2.chars
+  array_w1.each do |x|
+    array_w2.delete(x)
+  end
+  z = 0
+  wd1 = w1.chars
+  wd2 = w2.chars
+  z = 1 if wd1.last == wd2.last && wd1.first == wd2.first && w1.size != w2.size
+  array_w2.count + jumbled(w1,w2) + z
 end
 
 def jumbled(w1,w2)
-	if w1.chars.last == w2.chars.last
-		0
-	elsif w1.chars.first != w2.chars.first
-		2
-	else
-		1
-	end
+  if w1.chars.last == w2.chars.last
+    0
+  elsif w1.chars.first != w2.chars.first
+    2
+  else
+    1
+  end
 end
-
 
 # p fouls("cawffee", "coffee")  
 # p fouls("bog", "dog")
@@ -40,3 +38,4 @@ p kinda_like?("doge", "dog") == true
 p kinda_like?("bog", "dog") == true
 p kinda_like?("flow", "wolf") == false
 p kinda_like?("cawffee", "coffee") == false
+

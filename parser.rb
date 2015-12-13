d69a2abfe698315c377fdc285370c84eb3988fce
Samuel Priddy <spriddy@covermymeds.com>
2015-12-13 16:17:20 -0500

    Move match counting responsibility into own function

--- parser.rb
+++ parser.rb
@@ -7,14 +7,13 @@ end
 private
 
 def diff_count(fuzzy_word, exact_word)
-  common_chars = fuzzy_word.chars & exact_word.chars
+  [fuzzy_word.length, exact_word.length].max - match_count(fuzzy_word, exact_word)
+end
 
-  number_of_matches = 0
-  common_chars.each do |char|
-    number_of_matches += [fuzzy_word.count(char), exact_word.count(char)].min
+def match_count(fuzzy_word, exact_word)
+  fuzzy_word.chars.uniq.inject(0) do |memo, char|
+    memo += [fuzzy_word.count(char), exact_word.count(char)].min
   end
-
-  [fuzzy_word.length, exact_word.length].max - number_of_matches
 end
 
 def ends_jumbled?(fuzzy_word, exact_word)

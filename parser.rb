b2f24e4b87d62d164755b0285a0c4e2d4a220180
Matt Mohr <mmohr@covermymeds.com>
2016-03-10 09:31:28 -0500

    Extracted some boolean expressions into their own methods, removed the tertiary operator on 7

--- parser.rb
+++ parser.rb
@@ -4,18 +4,26 @@ def kinda_like?(fuzzy_word, exact_word)
   fuzzy = fuzzy_word.chars
   exact = exact_word.chars
   difference_counter = (fuzzy.length - exact.length).abs
-  common_length = fuzzy.length > exact.length ? exact.length : fuzzy.length
+  common_length = [fuzzy.length, exact.length].min
   (0...common_length).each do |i|
     difference_counter += 1 if fuzzy[i] != exact[i]
   end
-  if difference_counter.between?(2, 3) && difference_counter < common_length
-    flipped_ends?(fuzzy_word, exact_word) || middle_jumbled?(fuzzy, exact, common_length)
+  if maybe_false_positive?(difference_counter, common_length)
+    edge_case?(fuzzy, exact, common_length)
   else
     difference_counter < 3
   end
 end
 
 private
+
+def maybe_false_positive?(difference_counter, common_length)
+  difference_counter.between?(2, 3) && difference_counter < common_length
+end
+
+def edge_case?(fuzzy, exact, common_length)
+  flipped_ends?(fuzzy, exact) || middle_jumbled?(fuzzy, exact, common_length)
+end
 
 def flipped_ends?(fuzzy_word, exact_word)
   fuzzy_word[0] == exact_word.reverse[0] &&
